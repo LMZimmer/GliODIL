a7fd4a94ad502374d68256d5c1dfd0681c8e0c0d
LZimmer <zimmer.lucas.w@gmail.com>
2025-06-08 16:14:29 +0200

    Add Dockerfile and build script

--- infer_gliodil.py
+++ infer_gliodil.py
@@ -1,30 +1,49 @@
 import os
 import argparse
+import numpy as np
 import nibabel as nib
 
 
+def convert_tumorseg_labels(seg_dir):
+    os.makedirs("tmp", exist_ok=True)
+    temp_dir = "tmp/tumorseg_134.nii.gz"
+
+    seg = nib.load(seg_dir)
+    aff = np.eye(4)
+    seg_data = np.rint(seg.get_fdata()).astype(np.int32)
+
+    # GLIODIL:      1: non_enhancing, 3: edema, 4: enhancing
+    # BRATS (new):  1: non_enhancing, 2: edema, 3: enhancing
+    seg_data[(seg_data == 2) | (seg_data == 3)] += 1
+    seg_new = nib.Nifti1Image(seg_data, affine=aff)
+    nib.save(seg_new, temp_dir)
+
+    return temp_dir
+
+
 if __name__ == "__main__":
-    # Example:
-    # python single.py -cuda_device 0
-    # nohup python -u single.py -cuda_device 0 > tmp_single.out 2>&1 &
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-cuda_device", type=str, default="0", help="GPU id to run on.")
-    args = parser.parse_args()
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_device
+    CUDA_DEVICE = "0"
+    tumorSegmentationPath = "/mlcube_io0/Patient-00000/00000-tumorseg.nii.gz"
+    wmPath = "/mlcube_io0/Patient-00000/00000-wm.nii.gz"
+    gmPath = "/mlcube_io0/Patient-00000/00000-gm.nii.gz"
+    csfPath = "/mlcube_io0/Patient-00000/00000-csf.nii.gz"
+    savePath = "tmp"
+    logfile = "tmp/gliodil.log"
 
-    # tgm16, essential
-    patient_dir = "/mnt/Drive2/lucas/datasets/data_GliODIL_essential/data_716"
-    wmPath = os.path.join(patient_dir, "t1_wm.nii.gz")
-    gmPath = os.path.join(patient_dir, "t1_gm.nii.gz")
-    tumorSegmentationPath = os.path.join(patient_dir, "segm.nii.gz")
-    savePath = os.path.join(patient_dir, "pet_ess")
-    logfile = os.path.join(patient_dir, "tmp/gliodil.log")
-    petPath = os.path.join(patient_dir, "FET.nii.gz")
-    os.makedirs(savePath, exist_ok=True)
+    tumorSegmentationPath_134 = convert_tumorseg_labels(tumorSegmentationPath)
 
-    cmd = f'USEGPU=1 CUDA_VISIBLE_DEVICES={args.cuda_device} /home/home/lucas/projects/GliODIL/GliODIL.py --outdirectory "{savePath}" --optimizer adamn --postfix _pet__PDE1.0_ --lambda_pde_multiplier 1.0 --Nt 192 --Nx 48 --Ny 48 --Nz 48 --days 100 --history_every 1000 --report_every 1000 --epochs 9000 --plot_every 3000 --save_solution y --final_print y --code x --multigrid 1 --save_forward odil_res --save_forward2 full_trim_Gauss --initial_guess forward_character_dice_breaking --seg_path "{tumorSegmentationPath}" --wm_path "{wmPath}"  --gm_path "{gmPath}" --pet_path ""'
+    # Run without PET
+    cmd = f'USEGPU=1 CUDA_VISIBLE_DEVICES={CUDA_DEVICE} GliODIL.py --outdirectory "{savePath}" --optimizer adamn --postfix _pet__PDE1.0_ --lambda_pde_multiplier 1.0 --Nt 192 --Nx 48 --Ny 48 --Nz 48 --days 100 --history_every 1000 --report_every 1000 --epochs 9000 --plot_every 3000 --save_solution y --final_print y --code x --multigrid 1 --save_forward odil_res --save_forward2 full_trim_Gauss --initial_guess forward_character_dice_breaking --seg_path "{tumorSegmentationPath_134}" --wm_path "{wmPath}"  --gm_path "{gmPath}" --pet_path ""'
     
     print(cmd)
     os.system(cmd)
+
+    # Copy to mlcubeio1 and cleanup
+    pred_file = os.path.join(savePath+"x_pet__PDE1.0_", "tumorImage.nii.gz")
+    shutil.copy(pred_file, "/mlcube_io1/00000.nii.gz")
+
+    shutil.rmtree(savePath)
+    shutil.rmtree(savePath+"x_pet__PDE1.0_")
+
     print("Done.")
